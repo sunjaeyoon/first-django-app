dfa2da4aa29d7b0e7837da5d4087ebd341297574
jae <39254058+sunjaeyoon@users.noreply.github.com>
2021-01-22 11:24:57 -0800

    part three tutorial

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -2,9 +2,33 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+post =[
+    {
+        'author':'Hello',
+        'title':'Heyyyyy',
+        'content':'Whats Up',
+        'date_posted':'August 20, 2021'
+    },
+    {
+        'author':'Hello',
+        'title':'Heyyyyy',
+        'content':'Whats Up',
+        'date_posted':'August 20, 2021'
+    },
+    {
+        'author':'Hello',
+        'title':'Heyyyyy',
+        'content':'Whats Up',
+        'date_posted':'August 20, 2021'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': post
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
 
